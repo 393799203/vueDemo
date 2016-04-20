6c5b1766c701c36f76f97eaf02ee1cef9b45c4a6
qingmeng <qingmeng@mogujie.com>
2016-04-20 18:22:00 +0800

    rebase

---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,36 @@
+var path = require('path')
+var webpack = require('webpack')
+var merge = require('webpack-merge')
+var utils = require('./utils')
+var baseWebpackConfig = require('./webpack.base.conf')
+var TransferWebpackPlugin = require('transfer-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// add hot-reload related code to entry chunks
+Object.keys(baseWebpackConfig.entry).forEach(function (name) {
+  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
+})
+
+module.exports = merge(baseWebpackConfig, {
+  module: {
+    loaders: utils.styleLoaders()
+  },
+  // eval-source-map is faster for development
+  devtool: '#eval-source-map',
+  plugins: [
+    new TransferWebpackPlugin([
+      { from: 'src/assets',to: 'assets'},
+      { from: 'static',to: 'static'},
+    ],path.resolve(__dirname, '..')),
+    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    // https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+      inject: true
+    })
+  ]
+})
---
+++ src/main.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from './App'
+
+import Home from 'views/home/index'
+
+const FastClick = require('fastclick')
+FastClick.attach(document.body)
+
+Vue.use(Router)
+
+const router = new Router({
+  history: /vux.li/.test(window.location.href), // use history=false when testing
+  saveScrollPosition: true
+})
+
+router.map({
+  '/': {
+    component: Home
+  }
+})
+
+router.beforeEach(function (transition) {
+  if (/\/http/.test(transition.to.path)) {
+    let url = transition.to.path.split('http')[1]
+    window.location.href = `http${url}`
+  } else {
+    transition.next()
+  }
+})
+
+router.afterEach(function (transition) {
+  if (transition.to.fullPath !== '/demo') {
+    window.scrollTo(0, 0)
+  }
+})
+
+router.start(App, '#app')
